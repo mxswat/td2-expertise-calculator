c3a9986cd914b998f195647da6078a065f04ea5f
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2022-06-29 01:17:16 +0100

    Code works, next job is style

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,15 @@ function App() {
   const [rankProgress, setRankProgress] = useState(0);
   const [proficencyRank, setProficencyRank] = useState(0);
   const [barPercentage, setBarPercentage] = useState(0);
-  const [itemTypeDivided, setItemTypeDivided] = useState(0)
+  const [itemTypeDivided, setItemTypeDivided] = useState(8800)
 
   const handleItemType = (e: React.ChangeEvent<HTMLInputElement>) => {
     setItemTypeDivided(parseInt(e.target.value))
+  }
+
+  const onProficencyRankChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value)
+    setProficencyRank(isNaN(value) ? 0 : value)
   }
 
   const onRankProgressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,31 +32,38 @@ function App() {
   }
 
   const xpRequired = calcXPRequired(proficencyRank, rankProgress)
+  const itemsToDonate = Math.ceil(xpRequired / itemTypeDivided);
 
   return (
     <div className="App">
-      <h1>The Division 2 Expertise/Proficency Calculator</h1>
+      <h1 className='mb-4 text-xl text-center'>The Division 2 Expertise/Proficency Calculator</h1>
       <div className='flex flex-1 w-full flex-col'>
         <div className='flex justify-between'>
-          <span>Proficency Rank</span>
-          <input className='td2-input' type="number" placeholder='0' min={0} max={9} onChange={(e) => setProficencyRank(parseInt(e.target.value))} />
+          <span className='text-lg'>Proficency Rank</span>
+          <input className='text-lg td2-input' type="number" placeholder='0' min={0} max={9} step={1} onChange={onProficencyRankChange} />
         </div>
-        <div className='flex justify-between'>
-          <span>Rank progress </span>
-          <input className='td2-input' type="number" placeholder='0' min={0} max={88000} onChange={onRankProgressChange} />
+        <div className='flex justify-between mb-2'>
+          <span className='text-lg'>Rank progress </span>
+          <input className='text-lg td2-input' type="number" placeholder='0' min={0} max={88000} step={8800} onChange={onRankProgressChange} />
         </div>
-        <div className='progress-bar-container flex w-full h-2'>
+        <div className='progress-bar-container flex w-full border border-solid border-black'>
           <div className='bg-black h-2' style={{ width: `${barPercentage}%` }} />
         </div>
-        <div className='flex flex-col'>
-          <input type="radio" name="gender" value="8800" onChange={handleItemType} defaultChecked/>
-          <label htmlFor="8800">normal brands and gear set pieces</label>
-          <input type="radio" name="gender" value="44000" onChange={handleItemType} />
-          <label htmlFor="44000">weapons, improvised gear, exotics and named</label>
+
+        <div className='flex flex-col mt-4 mb-4'>
+          Calculate for:
+          <label className='flex items-center capitalize mb-1' htmlFor="8800">
+            <input className='mr-1' type="radio" name="gender" value="8800" onChange={handleItemType} defaultChecked />
+            normal brands and gear set pieces
+          </label>
+          <label className='flex items-center capitalize' htmlFor="44000">
+            <input className='mr-1' type="radio" name="gender" value="44000" onChange={handleItemType} />
+            weapons, improvised gear, exotics and named
+          </label>
         </div>
         <div className="flex flex-col">
           <span>XP Required = {xpRequired}</span>
-          <span>Items to donate = {xpRequired / itemTypeDivided}</span>
+          <span>Items to donate = {itemsToDonate}</span>
         </div>
       </div>
     </div>
